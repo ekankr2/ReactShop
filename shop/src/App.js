996c1a9307979488c7d5fe9f33ee4d2978fea60c
ekank <ekankr2@naver.com>
2021-10-14 21:23:20 +0900

    making item into a component

--- shop/src/App.js
+++ shop/src/App.js
@@ -41,28 +41,41 @@ function App() {
 
         <div className="container">
             <div className="row">
-                <div className="col-md-4">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-                    <h4>{shoes[0].title}</h4>
-                    <p>{shoes[0].content}</p>
-                    <p>{shoes[0].price}</p>
-                </div>
-                <div className="col-md-4">
-                    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
-                    <h4>{shoes[1].title}</h4>
-                    <p>{shoes[1].content}</p>
-                </div>
-                <div className="col-md-4">
-                    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
-                    <h4>{shoes[2].title}</h4>
-                    <p>{shoes[2].content}</p>
-                </div>
+                <Item shoes={shoes}></Item>
+
+                {/*<div className="col-md-4">*/}
+                {/*    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>*/}
+                {/*    <h4>{shoes[0].title}</h4>*/}
+                {/*    <p>{shoes[0].content}</p>*/}
+                {/*    <p>{shoes[0].price}</p>*/}
+                {/*</div>*/}
+                {/*<div className="col-md-4">*/}
+                {/*    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>*/}
+                {/*    <h4>{shoes[1].title}</h4>*/}
+                {/*    <p>{shoes[1].content}</p>*/}
+                {/*</div>*/}
+                {/*<div className="col-md-4">*/}
+                {/*    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>*/}
+                {/*    <h4>{shoes[2].title}</h4>*/}
+                {/*    <p>{shoes[2].content}</p>*/}
+                {/*</div>*/}
 
             </div>
         </div>
 
     </div>
   );
+}
+
+function Item(props){
+    return(
+        <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+            <h4>{props.shoes[0].title}</h4>
+            <p>{props.shoes[0].content}</p>
+            <p>{props.shoes[0].price}</p>
+        </div>
+    )
 }
 
 export default App;
