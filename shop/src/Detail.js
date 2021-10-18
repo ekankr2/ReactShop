60080dc55dad13790d4f58deb27a2f28522b3b48
ekank <ekankr2@naver.com>
2021-10-18 17:35:16 +0900

    React sass

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,5 +1,6 @@
 import { useHistory, useParams } from 'react-router-dom'
 import styled from 'styled-components'
+import './Detail.scss'
 
 let 박스 = styled.div`
     padding : 20px;
@@ -21,8 +22,11 @@ function Detail(props) {
     return(
         <div className="container">
             <박스>
-                <제목>Detail</제목>
+                <제목 className="red">Detail</제목>
             </박스>
+            <div className="my-alert2">
+                <p>재고가 얼마 남지 않았습니다</p>
+            </div>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
