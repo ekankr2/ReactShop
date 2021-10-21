467c01ef8ba9d9736769be819c38e61078a691c0
ekank <ekankr2@naver.com>
2021-10-21 20:48:11 +0900

    React redux reducer

--- shop/src/Cart.js
+++ shop/src/Cart.js
@@ -10,18 +10,27 @@ function Cart(props){
                     <thead>
                     <tr>
                         <th>#</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
+                        <th>상품명</th>
+                        <th>수량</th>
+                        <th>변경</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>{ props.state[0].name }</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
+                    {
+                        props.state.map((a,i)=>{
+                            return(
+                                <tr key={i}>
+                                    <td>{ a.id }</td>
+                                    <td>{ a.name }</td>
+                                    <td>{ a.quan }</td>
+                                    <td>
+                                        <button onClick={()=>{ props.dispatch({ type : '수량증가' })}}>+</button>
+                                        <button onClick={()=>{ props.dispatch({ type : '수량감소' })}}>-</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
                     </tbody>
                 </Table>
             </div>
