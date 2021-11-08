9bd03f454eac6755a45ef64bee9ef9288b4b6296
ekank <ekankr2@naver.com>
2021-11-09 07:38:00 +0900

    install firebase

--- carrot-react/src/App.js
+++ carrot-react/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase/compat";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.react_app_project_id,
+    storageBucket: process.env.react_app_storage_bucket,
+    messagingSenderId: process.env.react_app_messagin_id,
+    appId: process.env.REACT_APP_APP_ID
+})
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+
+      <section>
+        {user}
+      </section>
+
     </div>
   );
 }
