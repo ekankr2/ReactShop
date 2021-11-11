9b551bdb8c62b50d58a4319fd271e2d216103874
ekank <ekankr2@naver.com>
2021-11-12 05:49:32 +0900

    login page

--- carrot-react/src/App.js
+++ carrot-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
 
 import firebase from "firebase/compat";
 import 'firebase/firestore'
@@ -6,6 +7,7 @@ import 'firebase/auth'
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
@@ -22,19 +24,50 @@ const db = firebase.firestore()
 function App() {
 
   const [user] = useAuthState(auth)
+    console.log(user)
 
   return (
     <div className="App">
       <header>
-
+          <Navbar bg="light" variant="light">
+              <Container>
+                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                  <Nav className="me-auto">
+                      <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                      <Nav.Link as={Link} to='/detail'>Features</Nav.Link>
+                      <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  </Nav>
+              </Container>
+          </Navbar>
       </header>
 
-      <section>
-        {user}
-      </section>
+        <Switch>
+            <Route exact path="/">
+                <div>메인페이지에요</div>
+            </Route>
+            <Route path="/login">
+                {user ? <SignOut/> : <SignIn/>}
+            </Route>
+        </Switch>
+
 
     </div>
   );
+}
+
+function SignIn(){
+    const signInWithGoogle = ()=>{
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+    }
+    return(
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+    )
+}
+function SignOut(){
+    return auth.currentUser && (
+        <button onClick={()=> auth.signOut()}>Sign Out</button>
+    )
 }
 
 export default App;
