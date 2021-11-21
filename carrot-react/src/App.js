8baf3f5f70d62a73c6f233771ead49e3ef323490
ekank <ekankr2@naver.com>
2021-11-21 18:56:37 +0900

    SignIn component

--- carrot-react/src/App.js
+++ carrot-react/src/App.js
@@ -12,6 +12,7 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {useRef, useState} from "react";
 import {auth, db} from "./index";
 import Upload from "./components/Upload";
+import SignIn from "./components/SignIn";
 
 
 
@@ -65,41 +66,7 @@ function App() {
   );
 }
 
-function SignIn(){
-    let history = useHistory()
-    const emailRef = useRef()
-    const passwordRef = useRef()
 
-    const signInWithGoogle = ()=>{
-        const provider = new firebase.auth.GoogleAuthProvider()
-        auth.signInWithPopup(provider)
-    }
-
-    const login = ()=>{
-        let email = emailRef.current.value
-        let password = passwordRef.current.value
-
-        auth.signInWithEmailAndPassword(email, password).then((res)=>{
-            history.goBack()
-        })
-    }
-
-    return(
-        <>
-        <div className="container mt-3">
-            <div className="mb-3">
-                <input type="email" className="form-control" placeholder="이메일" ref={emailRef}/>
-            </div>
-            <div className="mb-3">
-                <input type="password" className="form-control" placeholder="비밀번호" ref={passwordRef}/>
-            </div>
-            <button className="btn btn-primary" id="register" onClick={login}>로그인</button>
-        </div>
-
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-        </>
-    )
-}
 function SignOut(){
     return auth.currentUser && (
         <button onClick={()=> auth.signOut()}>Sign Out</button>
