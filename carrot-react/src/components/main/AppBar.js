94165b53567483ea4cb7dabb1c628d9a5ca0b60c
ekank <ekankr2@naver.com>
2021-12-15 05:48:17 +0900

    container min height 92vh

--- carrot-react/src/components/main/AppBar.js
+++ carrot-react/src/components/main/AppBar.js
@@ -1,10 +1,13 @@
 import {Container, Navbar} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 export default function MyNavbar() {
+    let history = useHistory()
+
     return(
         <Navbar bg="light">
             <Container>
-                <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+                <Navbar.Brand onClick={()=>history.push('/')}>중고장터</Navbar.Brand>
             </Container>
         </Navbar>
     )
